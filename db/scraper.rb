c0f87e1b822aafb3fa795687f5a833859b7b2f31
coltenappleby <colten.appleby@gmail.com>
2021-05-24 15:28:46 -0400

    created serializers

--- db/scraper.rb
+++ db/scraper.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'json'
+require 'rest-client'
 
 # url = "https://www.indeed.com/jobs?q=$65,000&l=06820&jt=fulltime&sort=date&fromage=7"
 
@@ -42,8 +43,16 @@ query_url = "https://www.indeed.com/jobs?l=" + zip_code + "&radius=" + radius + 
 #     puts zip['city']
 # end
 
-html = URI.open(query_url)
-response = Nokogiri::HTML(html)
-content = response.css("div#searchCountPages").text.split("of ")[1].split(" jobs")[0].split(",").join()
+# html = URI.open(query_url)
+# response = Nokogiri::HTML(html)
+# content = response.css("div#searchCountPages").text.split("of ")[1].split(" jobs")[0].split(",").join()
 
-puts content
+# puts content
+
+res = RestClient.get("https://www.melissa.com/v2/lookups/newhomeowner/?zipcode=12180:&fmt=json")
+html = JSON.parse(res.body)
+puts html
+
+res = URI.open("https://www.melissa.com/v2/lookups/newhomeowner/?zipcode=12180:&fmt=json")
+html = JSON.parse(res.body)
+puts html
