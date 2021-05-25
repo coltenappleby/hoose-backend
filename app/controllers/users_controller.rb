7a32cbfdec0f46f6d6ea5904f33ec10423075093
coltenappleby <colten.appleby@gmail.com>
2021-05-24 23:53:34 -0400

    more serializers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,16 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            render json: @user
+        else
+            render json: @user.errors.full_messages
+        end
+    end
+
+
     private
 
     def user_params
--- app/serializers/zip_codes_serializers.rb
+++ app/serializers/zip_codes_serializers.rb
@@ -1,5 +1,5 @@
 class ZipCodeSerializer < ActiveModel::Serializer
-    attributes :id, :zip, :name, :population, :state, :county, :job_searches
+    attributes :id, :zip, :name, :population, :state, :county
 
     has_many :job_searches
     # has_many :favorited_zips
