0584d307d26ee8eda55d03d1f3e31555a3425216
coltenappleby <colten.appleby@gmail.com>
2021-05-27 14:41:28 -0400

    added created_at serializer data for jobsearch and edit how remote is delivered to front end vis serializer

--- app/serializers/job_search_serializer.rb
+++ app/serializers/job_search_serializer.rb
@@ -1,6 +1,18 @@
 class JobSearchSerializer < ActiveModel::Serializer
-    attributes :id, :remote, :salary_estimate, :search_term, :radius, :job_type, :experience_level, :date_posted, :number_of_posts, :zip_code
+    attributes :id, :remote, :salary_estimate, :search_term, :radius, :job_type, :experience_level, :date_posted, :number_of_posts, :zip_code, :search_date
 
     belongs_to :user
     # belongs_to :zip_code
+
+    def search_date
+        object.created_at.strftime("%Y-%m-%d ")
+    end
+
+    def remote
+        if object.remote == false
+            return "not-remote"
+        else
+            return "remote"
+        end
+    end
 end
